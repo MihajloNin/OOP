8b1d678250c0b6f3bcc8e5469467d6b385fa03b0
MihajloNin <112658443+MihajloNin@users.noreply.github.com>
2023-03-26 11:50:36 +0200

    Add files via upload

---
+++ OOP - Moving Label/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace OOP___Moving_Label
+{
+    partial class FormIgrica
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlKretanje = new System.Windows.Forms.Panel();
+            this.lblKretac = new System.Windows.Forms.Label();
+            this.pnlIspisAkcija = new System.Windows.Forms.Panel();
+            this.lblBrGreski = new System.Windows.Forms.Label();
+            this.lblGreske = new System.Windows.Forms.Label();
+            this.lblBrDole = new System.Windows.Forms.Label();
+            this.lblBrGore = new System.Windows.Forms.Label();
+            this.lblBrDesno = new System.Windows.Forms.Label();
+            this.lblBrLevo = new System.Windows.Forms.Label();
+            this.lblDole = new System.Windows.Forms.Label();
+            this.lblGore = new System.Windows.Forms.Label();
+            this.lblDesno = new System.Windows.Forms.Label();
+            this.lblLevo = new System.Windows.Forms.Label();
+            this.listAkcije = new System.Windows.Forms.ListView();
+            this.pnlKomande = new System.Windows.Forms.Panel();
+            this.BtnDole = new System.Windows.Forms.Button();
+            this.btnGore = new System.Windows.Forms.Button();
+            this.btnDesno = new System.Windows.Forms.Button();
+            this.btnLevo = new System.Windows.Forms.Button();
+            this.pnlKretanje.SuspendLayout();
+            this.pnlIspisAkcija.SuspendLayout();
+            this.pnlKomande.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlKretanje
+            // 
+            this.pnlKretanje.Controls.Add(this.lblKretac);
+            this.pnlKretanje.Location = new System.Drawing.Point(12, 12);
+            this.pnlKretanje.Name = "pnlKretanje";
+            this.pnlKretanje.Size = new System.Drawing.Size(889, 407);
+            this.pnlKretanje.TabIndex = 0;
+            // 
+            // lblKretac
+            // 
+            this.lblKretac.AutoSize = true;
+            this.lblKretac.Location = new System.Drawing.Point(309, 163);
+            this.lblKretac.Name = "lblKretac";
+            this.lblKretac.Size = new System.Drawing.Size(46, 17);
+            this.lblKretac.TabIndex = 0;
+            this.lblKretac.Text = "label1";
+            // 
+            // pnlIspisAkcija
+            // 
+            this.pnlIspisAkcija.Controls.Add(this.lblBrGreski);
+            this.pnlIspisAkcija.Controls.Add(this.lblGreske);
+            this.pnlIspisAkcija.Controls.Add(this.lblBrDole);
+            this.pnlIspisAkcija.Controls.Add(this.lblBrGore);
+            this.pnlIspisAkcija.Controls.Add(this.lblBrDesno);
+            this.pnlIspisAkcija.Controls.Add(this.lblBrLevo);
+            this.pnlIspisAkcija.Controls.Add(this.lblDole);
+            this.pnlIspisAkcija.Controls.Add(this.lblGore);
+            this.pnlIspisAkcija.Controls.Add(this.lblDesno);
+            this.pnlIspisAkcija.Controls.Add(this.lblLevo);
+            this.pnlIspisAkcija.Controls.Add(this.listAkcije);
+            this.pnlIspisAkcija.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnlIspisAkcija.Location = new System.Drawing.Point(907, 0);
+            this.pnlIspisAkcija.Name = "pnlIspisAkcija";
+            this.pnlIspisAkcija.Size = new System.Drawing.Size(279, 541);
+            this.pnlIspisAkcija.TabIndex = 1;
+            // 
+            // lblBrGreski
+            // 
+            this.lblBrGreski.AutoSize = true;
+            this.lblBrGreski.Location = new System.Drawing.Point(123, 265);
+            this.lblBrGreski.Name = "lblBrGreski";
+            this.lblBrGreski.Size = new System.Drawing.Size(0, 17);
+            this.lblBrGreski.TabIndex = 10;
+            // 
+            // lblGreske
+            // 
+            this.lblGreske.AutoSize = true;
+            this.lblGreske.Location = new System.Drawing.Point(14, 265);
+            this.lblGreske.Name = "lblGreske";
+            this.lblGreske.Size = new System.Drawing.Size(82, 17);
+            this.lblGreske.TabIndex = 9;
+            this.lblGreske.Text = "Broj Greski:";
+            // 
+            // lblBrDole
+            // 
+            this.lblBrDole.AutoSize = true;
+            this.lblBrDole.Location = new System.Drawing.Point(76, 161);
+            this.lblBrDole.Name = "lblBrDole";
+            this.lblBrDole.Size = new System.Drawing.Size(0, 17);
+            this.lblBrDole.TabIndex = 8;
+            // 
+            // lblBrGore
+            // 
+            this.lblBrGore.AutoSize = true;
+            this.lblBrGore.Location = new System.Drawing.Point(76, 117);
+            this.lblBrGore.Name = "lblBrGore";
+            this.lblBrGore.Size = new System.Drawing.Size(0, 17);
+            this.lblBrGore.TabIndex = 7;
+            // 
+            // lblBrDesno
+            // 
+            this.lblBrDesno.AutoSize = true;
+            this.lblBrDesno.Location = new System.Drawing.Point(85, 77);
+            this.lblBrDesno.Name = "lblBrDesno";
+            this.lblBrDesno.Size = new System.Drawing.Size(0, 17);
+            this.lblBrDesno.TabIndex = 6;
+            // 
+            // lblBrLevo
+            // 
+            this.lblBrLevo.AutoSize = true;
+            this.lblBrLevo.Location = new System.Drawing.Point(76, 32);
+            this.lblBrLevo.Name = "lblBrLevo";
+            this.lblBrLevo.Size = new System.Drawing.Size(0, 17);
+            this.lblBrLevo.TabIndex = 5;
+            // 
+            // lblDole
+            // 
+            this.lblDole.AutoSize = true;
+            this.lblDole.Location = new System.Drawing.Point(14, 161);
+            this.lblDole.Name = "lblDole";
+            this.lblDole.Size = new System.Drawing.Size(41, 17);
+            this.lblDole.TabIndex = 4;
+            this.lblDole.Text = "Dole:";
+            // 
+            // lblGore
+            // 
+            this.lblGore.AutoSize = true;
+            this.lblGore.Location = new System.Drawing.Point(14, 117);
+            this.lblGore.Name = "lblGore";
+            this.lblGore.Size = new System.Drawing.Size(44, 17);
+            this.lblGore.TabIndex = 3;
+            this.lblGore.Text = "Gore:";
+            // 
+            // lblDesno
+            // 
+            this.lblDesno.AutoSize = true;
+            this.lblDesno.Location = new System.Drawing.Point(14, 73);
+            this.lblDesno.Name = "lblDesno";
+            this.lblDesno.Size = new System.Drawing.Size(53, 17);
+            this.lblDesno.TabIndex = 2;
+            this.lblDesno.Text = "Desno:";
+            // 
+            // lblLevo
+            // 
+            this.lblLevo.AutoSize = true;
+            this.lblLevo.Location = new System.Drawing.Point(14, 32);
+            this.lblLevo.Name = "lblLevo";
+            this.lblLevo.Size = new System.Drawing.Size(43, 17);
+            this.lblLevo.TabIndex = 1;
+            this.lblLevo.Text = "Levo:";
+            // 
+            // listAkcije
+            // 
+            this.listAkcije.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listAkcije.HideSelection = false;
+            this.listAkcije.Location = new System.Drawing.Point(185, 3);
+            this.listAkcije.Name = "listAkcije";
+            this.listAkcije.Size = new System.Drawing.Size(82, 533);
+            this.listAkcije.TabIndex = 0;
+            this.listAkcije.UseCompatibleStateImageBehavior = false;
+            // 
+            // pnlKomande
+            // 
+            this.pnlKomande.Controls.Add(this.BtnDole);
+            this.pnlKomande.Controls.Add(this.btnGore);
+            this.pnlKomande.Controls.Add(this.btnDesno);
+            this.pnlKomande.Controls.Add(this.btnLevo);
+            this.pnlKomande.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlKomande.Location = new System.Drawing.Point(0, 425);
+            this.pnlKomande.Name = "pnlKomande";
+            this.pnlKomande.Size = new System.Drawing.Size(907, 116);
+            this.pnlKomande.TabIndex = 2;
+            // 
+            // BtnDole
+            // 
+            this.BtnDole.Location = new System.Drawing.Point(255, 81);
+            this.BtnDole.Name = "BtnDole";
+            this.BtnDole.Size = new System.Drawing.Size(75, 23);
+            this.BtnDole.TabIndex = 3;
+            this.BtnDole.Text = "Dole";
+            this.BtnDole.UseVisualStyleBackColor = true;
+            this.BtnDole.Click += new System.EventHandler(this.BtnDole_Click);
+            // 
+            // btnGore
+            // 
+            this.btnGore.Location = new System.Drawing.Point(255, 15);
+            this.btnGore.Name = "btnGore";
+            this.btnGore.Size = new System.Drawing.Size(75, 23);
+            this.btnGore.TabIndex = 2;
+            this.btnGore.Text = "Gore";
+            this.btnGore.UseVisualStyleBackColor = true;
+            this.btnGore.Click += new System.EventHandler(this.btnGore_Click);
+            // 
+            // btnDesno
+            // 
+            this.btnDesno.Location = new System.Drawing.Point(388, 49);
+            this.btnDesno.Name = "btnDesno";
+            this.btnDesno.Size = new System.Drawing.Size(75, 23);
+            this.btnDesno.TabIndex = 1;
+            this.btnDesno.Text = "Desno";
+            this.btnDesno.UseVisualStyleBackColor = true;
+            this.btnDesno.Click += new System.EventHandler(this.btnDesno_Click);
+            // 
+            // btnLevo
+            // 
+            this.btnLevo.Location = new System.Drawing.Point(121, 49);
+            this.btnLevo.Name = "btnLevo";
+            this.btnLevo.Size = new System.Drawing.Size(75, 23);
+            this.btnLevo.TabIndex = 0;
+            this.btnLevo.Text = "Levo";
+            this.btnLevo.UseVisualStyleBackColor = true;
+            this.btnLevo.Click += new System.EventHandler(this.btnLevo_Click);
+            // 
+            // FormIgrica
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1186, 541);
+            this.Controls.Add(this.pnlKomande);
+            this.Controls.Add(this.pnlIspisAkcija);
+            this.Controls.Add(this.pnlKretanje);
+            this.Name = "FormIgrica";
+            this.Text = "Igrica";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlKretanje.ResumeLayout(false);
+            this.pnlKretanje.PerformLayout();
+            this.pnlIspisAkcija.ResumeLayout(false);
+            this.pnlIspisAkcija.PerformLayout();
+            this.pnlKomande.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlKretanje;
+        private System.Windows.Forms.Panel pnlIspisAkcija;
+        private System.Windows.Forms.Panel pnlKomande;
+        private System.Windows.Forms.Label lblKretac;
+        private System.Windows.Forms.Button BtnDole;
+        private System.Windows.Forms.Button btnGore;
+        private System.Windows.Forms.Button btnDesno;
+        private System.Windows.Forms.Button btnLevo;
+        private System.Windows.Forms.ListView listAkcije;
+        private System.Windows.Forms.Label lblLevo;
+        private System.Windows.Forms.Label lblBrDole;
+        private System.Windows.Forms.Label lblBrGore;
+        private System.Windows.Forms.Label lblBrDesno;
+        private System.Windows.Forms.Label lblBrLevo;
+        private System.Windows.Forms.Label lblDole;
+        private System.Windows.Forms.Label lblGore;
+        private System.Windows.Forms.Label lblDesno;
+        private System.Windows.Forms.Label lblBrGreski;
+        private System.Windows.Forms.Label lblGreske;
+    }
+}
+
---
+++ OOP - Moving Label/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP___Moving_Label
+{
+    public partial class FormIgrica : Form
+    {
+        public FormIgrica()
+        {
+            InitializeComponent();
+
+            Init();
+        }
+
+        private void Init()
+        {
+            int x = pnlKretanje.Width;
+            int y = pnlKretanje.Height;
+            lblKretac.Location = new Point(x / 2, y / 2);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+        int pomeranjegore, pomeranjedole, pomeranjelevo, pomeranjedesno, brgreski;
+
+        private void btnGore_Click(object sender, EventArgs e)
+        {
+            if (lblKretac.Location.Y - 10 < pnlKretanje.Location.Y)
+            {
+                MessageBox.Show("Izlazi van okvira panela");
+                brgreski++;
+                lblBrGreski.Text = Convert.ToString(brgreski);
+            }
+            else
+            {
+                lblKretac.Location = new Point(lblKretac.Location.X, lblKretac.Location.Y - 10);
+                listAkcije.Items.Add("Gore");
+                pomeranjegore++;
+                lblBrGore.Text = Convert.ToString(pomeranjegore);
+            }
+        }
+
+        private void btnLevo_Click(object sender, EventArgs e)
+        {
+            if (lblKretac.Location.X - 10 < pnlKretanje.Location.X)
+            {
+                MessageBox.Show("Izlazi van okvira panela");
+                brgreski++;
+                lblBrGreski.Text = Convert.ToString(brgreski);
+            }
+            else
+            {
+                lblKretac.Location = new Point(lblKretac.Location.X - 10, lblKretac.Location.Y);
+                listAkcije.Items.Add("Levo");
+                pomeranjelevo++;
+                lblBrLevo.Text = Convert.ToString(pomeranjelevo);
+            }
+        }
+
+        private void BtnDole_Click(object sender, EventArgs e)
+        {
+            if(lblKretac.Location.Y + 10 > pnlKomande.Location.Y - 45)
+            {
+                MessageBox.Show("Izlazi van okvira panela");
+                brgreski++;
+                lblBrGreski.Text = Convert.ToString(brgreski);
+            }
+            else
+            {
+                lblKretac.Location = new Point(lblKretac.Location.X, lblKretac.Location.Y + 10);
+                listAkcije.Items.Add("Dole");
+                pomeranjedole++;
+                lblBrDole.Text = Convert.ToString(pomeranjedole);
+            }
+            
+        }
+
+        private void btnDesno_Click(object sender, EventArgs e)
+        {
+            if(lblKretac.Location.X + 10 > pnlIspisAkcija.Location.X - 45)
+            {
+                MessageBox.Show("Izlazi van okvira panela");
+                brgreski++;
+                lblBrGreski.Text = Convert.ToString(brgreski);
+            }
+            else
+            {
+                lblKretac.Location = new Point(lblKretac.Location.X+10, lblKretac.Location.Y);
+                listAkcije.Items.Add("Desno");
+                pomeranjedesno++;
+                lblBrDesno.Text = Convert.ToString(pomeranjedesno);
+            }
+            
+        }
+    }
+}
